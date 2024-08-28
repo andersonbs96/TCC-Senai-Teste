325ff1f169221d68d8a8fb848b8d9fa9e435d559
Anderson Brito Santos <141651296+andersonbs96@users.noreply.github.com>
2024-08-28 10:11:59 -0300

    Add files via upload

--- volun-senai-main/src/components/Cadastrar.jsx
+++ volun-senai-main/src/components/Cadastrar.jsx
@@ -2,7 +2,15 @@ import React from "react"
 import Logo from "./../assets/logos/logo-big.svg";
 import "./../css/Cadastrar.css";
 
-const Cadastrar = () => {
+const Cadastrar = ({FormData, setFormData, handleRegister}) => {
+    const handleChange = (e) => {
+        const {id, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [id]: value,
+        }));
+    };
+
     return (
         <>
             <main className="cadastrar-container">
@@ -10,18 +18,33 @@ const Cadastrar = () => {
                     <img className="cadastrar-logo" src={Logo} alt="Logo"/>
                     <div className="cadastrar-input-container">
                         <label htmlFor="cadastrar-email">E-mail: </label>
-                        <input id="cadastrar-email" type="email"/>
+                        <input 
+                            id="cadastrar-email" 
+                            type="email" 
+                            value={FormData["cadastrar-email"] || ""}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="cadastrar-input-container">
                         <label htmlFor="cadastrar-senha">Senha: </label>
-                        <input id="cadastrar-senha" type="password"/>
+                        <input 
+                            id="cadastrar-senha" 
+                            type="password"
+                            value={FormData["cadastrar-senha"] || ""}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="cadastrar-input-container">
                         <label htmlFor="confirmar-senha">Confirmar a Senha: </label>
-                        <input id="confirmar-senha" type="password"/>
+                        <input 
+                            id="confirmar-senha" 
+                            type="password" 
+                            value={FormData["confirmar-senha"] || ""}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div className="cadastrar-botao-container">
-                        <button>CADASTRAR</button>
+                        <button onClick={handleRegister}>CADASTRAR</button>
                     </div>
                 </div>
             </main>
